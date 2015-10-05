f49286b0e0bcf8518755fffa7ab5ac8684fdf4cc
Matthew Leibowitz <mattleibow@live.com>
2015-10-05 20:06:43 +0200

    Update component bits/images

--- StickyHeader/Properties/AssemblyInfo.cs
+++ StickyHeader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StickyHeader")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Xamarin Inc 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
